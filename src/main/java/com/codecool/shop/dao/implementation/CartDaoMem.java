53ff116c5a235d29f1540c637a7212ee63bc7a44
weronikawalczak <weronikawalczak989@gmail.com>
2019-10-07 20:57:22 +0200

    Fixed passing product id from form to cart by POST

--- src/main/java/com/codecool/shop/dao/implementation/CartDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/CartDaoMem.java
@@ -58,4 +58,9 @@ public class CartDaoMem implements CartDao {
     public float getSum() {
         return sum;
     }
+
+    @Override
+    public int getNumberOfProducts() {
+        return 0;
+    }
 }
